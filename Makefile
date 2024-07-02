2507f2eda3c3ba0a686e1d8e369bd69fc1bce9e0
Kshyun_github <kusukusukusun@gmail.com>
2024-07-02 17:08:40 +0900

    commit message

--- Makefile
+++ Makefile
@@ -1,1 +1,8 @@
-make your own Makefile.
+main.o : main.cc GF_field.hh
+	g++ -c main.cc
+
+GF_field.o : GF_field.cc GF_field.hh
+	g++ -c GF_field.cc
+
+GF_field : main.o GF_field.o
+	g++ GF_field.o main.o -o GF_field
--- src/main.cc
+++ src/main.cc
@@ -1,20 +1,19 @@
 #include "GF_field.hh"
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main(void) {
-    srand(time(NULL));
+    // srand(time(NULL));
 
-    // example code
-    int random_exp = (rand() % GF_ELEMENT.get_size());
-    GF_ELEMENT a(random_exp);
-    int random_exp = (rand() % GF_ELEMENT.get_size());
-    GF_ELEMENT b(random_exp);
+    // 임의의 갈루아 필드 GF(2^8) 내의 값 두 개 사용
+    GF_ELEMENT a(0x57); 
+    GF_ELEMENT b(0x83); 
 
-    std::cout << a + b << std::endl;
-    std::cout << a * b << std::endl;
-    std::cout << a - b << std::endl;
-    std::cout << a / b << std::endl;
-    //std::cout << a % b << std::endl;
-
+    // 사칙연산 예시 수행
+    std::cout << "a + b = " << (a + b) << std::endl;
+    std::cout << "a * b = " << (a * b) << std::endl;
+    std::cout << "a - b = " << (a - b) << std::endl;
+    std::cout << "a / b = " << (a / b) << std::endl;
     return 0;
 }
